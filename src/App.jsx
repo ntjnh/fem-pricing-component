f99ba1a05bc142f803ca659a3bf5f294a9a114ec
Nate J.M <nate@natejonah.com>
2023-08-21 21:04:58 +0100

    Add content and assets for challenge
    Also installs sass

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import Attr from './components/Attr'
+import './assets/scss/App.scss'
+
+function App() {
+    return (
+        <>
+            <div>
+                Our Pricing
+                Annually
+                Monthly
+
+                Basic
+                &dollar;19.99
+                &dollar;199.99
+                500 GB Storage
+                2 Users Allowed
+                Send up to 3 GB
+                Learn More
+
+                Professional
+                &dollar;24.99
+                &dollar;249.99
+                1 TB Storage
+                5 Users Allowed
+                Send up to 10 GB
+                Learn More
+
+                Master
+                &dollar;39.99
+                &dollar;399.99
+                2 TB Storage
+                10 Users Allowed
+                Send up to 20 GB
+                Learn More
+            </div>
+
+            <Attr />
+            
+        </>
+    )
+}
+
+export default App
---
+++ src/components/Attr.jsx
@@ -0,0 +1,10 @@
+import './Attr.scss'
+
+export default function Attr() {
+    return (
+        <div className="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+            Coded by <a href="https://natejonah.com" target="_blank" rel="noreferrer">Nate</a>.
+        </div>
+    )
+}
