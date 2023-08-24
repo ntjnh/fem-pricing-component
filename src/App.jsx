7faa4620098787f329c8cc7abbd879bc5123dddd
Nate J.M <nate@natejonah.com>
2023-08-24 22:22:19 +0100

    Import plan data and set up toggle functionality
    Plan data is passed to component instances via props

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,43 @@
 import Attr from './components/Attr'
 import './assets/scss/App.scss'
+import planData from './plans.json'
+import Plan from './components/Plan'
+import { useState } from 'react'
 
 function App() {
+    const [billingFrequency, setBillingFrequency] = useState('monthly')
+
+    const plans = planData.map((plan, i) => {
+        return (
+            <Plan 
+                key={i} 
+                plan={plan.plan} 
+                price={plan.prices[billingFrequency]} 
+                features={plan.features}
+            />
+        )
+    })
+
+    function handleClick() {
+        if (billingFrequency === 'monthly') {
+            setBillingFrequency('annually')
+        } else {
+            setBillingFrequency('monthly')
+        }
+    }
+
     return (
         <>
             <div>
-                Our Pricing
-                Annually
-                Monthly
+                <h2>Our Pricing</h2>
 
-                Basic
-                &dollar;19.99
-                &dollar;199.99
-                500 GB Storage
-                2 Users Allowed
-                Send up to 3 GB
-                Learn More
+                <div>
+                    <p>Annually</p>
+                    <button className="price-toggle" type="button" onClick={handleClick}>toggle</button>
+                    <p>Monthly</p>
+                </div>
 
-                Professional
-                &dollar;24.99
-                &dollar;249.99
-                1 TB Storage
-                5 Users Allowed
-                Send up to 10 GB
-                Learn More
-
-                Master
-                &dollar;39.99
-                &dollar;399.99
-                2 TB Storage
-                10 Users Allowed
-                Send up to 20 GB
-                Learn More
+                {plans}
             </div>
 
             <Attr />
