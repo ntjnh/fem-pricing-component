2f193e2aca678cc893ba93bc7d91bfd5db7f737e
Nate J.M <nate@natejonah.com>
2023-08-26 21:42:58 +0100

    Structure and style attribution

--- src/components/Attr.jsx
+++ src/components/Attr.jsx
@@ -2,9 +2,11 @@ import './Attr.scss'
 
 export default function Attr() {
     return (
-        <div className="attribution">
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
-            Coded by <a href="https://natejonah.com" target="_blank" rel="noreferrer">Nate</a>.
-        </div>
+        <footer className="attribution">
+            <div>
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.<br />
+                Coded by <a href="https://natejonah.com" target="_blank" rel="noreferrer">Nate</a>.
+            </div>
+        </footer>
     )
 }
